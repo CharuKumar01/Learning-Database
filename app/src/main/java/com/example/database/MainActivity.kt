9fd23bb4c8f875a1726afaecc73eeeb3b901c67c
CharuKumar01 <kumarcharu83@gmail.com>
2025-04-13 19:19:23 +0530

    create single-ton database

--- app/src/main/java/com/example/database/MainActivity.kt
+++ app/src/main/java/com/example/database/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.example.database
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import com.example.database.databinding.ActivityMainBinding
 import com.google.android.gms.common.util.DataUtils
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bind : ActivityMainBinding
@@ -28,11 +32,15 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        database = Room.databaseBuilder(applicationContext, ContactDatabase::class.java, "contactDB")
-                    .build()
+        database = ContactDatabase.getDatabase(this)
 
-        lifecycleScope.launch(Dispatchers.IO) {
-            database.contactDao().insertContact(Contact(0, "Charu", "7247871944"))
+        lifecycleScope.launch {
+            lifecycleScope.launch {
+                database.contactDao().insertContact(Contact(0, "Charu", "7247871944", Date()))
+            }
         }
+        database.contactDao().getContact().observe(this, Observer { list ->
+            Log.d("DB_CHECK", list.toString())
+        })
     }
 }
